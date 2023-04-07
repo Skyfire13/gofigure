5b8afe1a27af52aec18913d5034d4833abf06f02
Skyfire13 <alanjamesfuller@gmail.com>
2023-04-06 21:43:49 -0500

    Working as intended

--- config.sh
+++ config.sh
@@ -1,31 +1,56 @@
 #!/bin/sh
 
-
 local_path="/home/$USER/.config"
 chezmoi_path="/home/$USER/.local/share/chezmoi/dot_config"
 
-
 export CONFIGS=$(cat <<END
-i3wm:$local_path/i3/config
-lf:$chezmoi_path/lf/lfrc
-kitty:$local_path/kitty/kitty.conf
-picom:$local_path/picom/picom.conf
-fish:$chezmoi_path/private_fish/config.fish
-mimeapps:$chezmoi_path/mimeapps.list
-External_Drives:/media/
-nvim:$chezmoi_path/nvim/
+i3wm
+lf
+kitty
+picom
+fish
+mimeapps
+External_Drives
+nvim
 END
 )
-
 
 GUM_CONFIG="\
     --cursor=* \
     --item.bold"
 
 
-KEY=$(echo "$CONFIGS" | cut -d':' -f1 | gum choose $GUM_CONFIG)
+KEY=$(echo "$CONFIGS" | gum choose $GUM_CONFIG)
 CHOICE=$(echo "$CONFIGS" | grep $KEY | cut -d':' -f2)
 
+# Create a switch case for each config
+case $CHOICE in
+    i3wm)
+        $EDITOR $local_path/i3/config
+        ;;
+    lf)
+        $EDITOR $chezmoi_path/lf/lfrc
+        ;;
+    kitty)
+        $EDITOR $local_path/kitty/kitty.conf
+        ;;
+    picom)
+        $EDITOR $local_path/picom/picom.conf
+        ;;
+    fish)
+        $EDITOR $chezmoi_path/private_fish/config.fish
+        ;;
+    mimeapps)
+        $EDITOR $chezmoi_path/mimeapps.list
+        ;;
+    External_Drives)
+        lf /media/
+        ;;
+    nvim)
+        lf $chezmoi_path/nvim/
+        ;;
 
-$EDITOR $CHOICE
-
+    *)
+        echo "Usage: config.sh [i3wm|lf|kitty|picom|fish|mimeapps|External_Drives|nvim]"
+        ;;
+esac
